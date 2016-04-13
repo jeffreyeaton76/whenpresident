100c795c5dc588c501a6c07d4fd1ca85759130de
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-13 13:14:59 -0400

    displays candidates at /index

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,52 @@
 "use strict";
 
 (function(){
-  
   angular
   .module("candidates", [
     "ui.router",
     "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    Router
+  ])
+  .factory("Candidate", [
+    "$resource",
+    candidateFactoryFunction
+  ])
+  .controller("candidateIndexCtrl", [
+    "Candidate",
+    candidateIndexCtrl
   ]);
+
+  function Router ($stateProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("welcome", {
+      url: "/",
+      templateUrl: "/assets/html/app-welcome.html"
+    })
+    .state("index", {
+      url: "/index",
+      templateUrl: "/assets/html/candidates-index.html",
+      controller: "candidateIndexCtrl",
+      controllerAs: "indexVM"
+    });
+  }
+
+  function candidateFactoryFunction($resource){
+    var Candidate = $resource("/api/candidates/:name", {}, {
+      update: {method: "PUT"}
+    });
+    Candidate.all = Candidate.query();
+    return Candidate;
+  }
+
+  function candidateIndexCtrl(Candidate){
+    var vm = this;
+    vm.candidates = Candidate.all;
+  }
+
 
 })();
